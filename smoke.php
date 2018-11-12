1b7c7817dde246b6626e8d0fa5988671143c597d
Eric Mann <eric@eamann.com>
2018-11-11 21:35:29 -0800

    Clean up smoke test

--- smoke.php
+++ smoke.php
@@ -7,16 +7,24 @@ if (-1 === version_compare(phpversion(), '7.1')) {
     echo sprintf("Running PHP %s. GREAT!\r\n", phpversion());
 }
 
-if (-1 === version_compare(phpversion('sodium'), '7.1')) {
+if (! function_exists('sodium_add')) {
     echo "This project requires Libsodium support. Please install it to continue...\r\n";
     exit(1);
 } else {
     echo "Libsodium is available. Great!\r\n";
 }
 
-if (-1 === version_compare(phpversion('sqlite3'), '7.1')) {
+if ( ! class_exists('SQLite3')) {
     echo "This project requires SQLite support. Please install it to continue...\r\n";
     exit(1);
 } else {
     echo "SQLite is available. Great!\r\n";
+}
+
+try {
+    new \PDO('sqlite::memory:');
+    echo "PDO drivers for SQLite are available. Great!\r\n";
+} catch (\Exception $exception) {
+    echo "This project requires PDO driver support for SQLite. Please install it to continue...\r\n";
+    exit(1);
 }
